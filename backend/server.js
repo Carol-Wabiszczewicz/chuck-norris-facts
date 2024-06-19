a2b6b0611c1999631c1d5de17ea12aed29386f78
Andre Fontes <carol.wabiszczewicz@gmail.com>
2024-06-19 18:52:48 -0300

    Set up basic Express server to fetch Chuck Norris facts

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/api/chuck-norris', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.chucknorris.io/jokes/random');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
