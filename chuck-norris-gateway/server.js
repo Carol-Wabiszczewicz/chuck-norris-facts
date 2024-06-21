982fee2af3a9d064762af1c98582ac2261ca164d
Andre Fontes <carol.wabiszczewicz@gmail.com>
2024-06-21 04:40:32 -0300

    FIX: communication with API hosting for data query, using Heroku

--- chuck-norris-gateway/server.js
+++ chuck-norris-gateway/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const cool = require('cool-ascii-faces');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 
